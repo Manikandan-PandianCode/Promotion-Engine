0c657f38cc634162457917555a1ebbaaac3eff4e
Manikandan <manikandan.p@sas.dk>
2020-09-22 18:06:02 +0200

    TDD - sending the number of items from checkout junit class and setting into product class

--- Promotion/src/main/java/Checkout.java
+++ Promotion/src/main/java/Checkout.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Checkout {
 
 	public void addProductInCart(int numberofItems) {
-		
+		List<Product> products = new ArrayList<Product>();
+		Scanner skuInput = new Scanner(System.in);
+		for (int i = 1; i < numberofItems; i++) {
+			System.out.println("Enter the SKUids " + i + "Out of " + numberofItems);
+			char skuid = skuInput.next().charAt(0);
+			Product product = new Product(skuid);
+			products.add(product);
+		}
 	}
 }
---
+++ Promotion/src/main/java/Product.java
@@ -0,0 +1,17 @@
+
+public class Product {
+
+	Product(char skuid) {
+		this.skuid = skuid;
+	}
+
+	private char skuid;
+
+	public char getSkuid() {
+		return skuid;
+	}
+
+	public void setSkuid(char skuid) {
+		this.skuid = skuid;
+	}
+}

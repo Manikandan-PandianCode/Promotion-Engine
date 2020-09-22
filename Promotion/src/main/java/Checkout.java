7bc9379a2ac7a2f15c34754fb0b2afaa71443cd4
Manikandan <manikandan.p@sas.dk>
2020-09-22 18:59:49 +0200

    Code refactor is done for scenario A and Scenario B

--- Promotion/src/main/java/Checkout.java
+++ Promotion/src/main/java/Checkout.java
@@ -7,11 +7,23 @@ public class Checkout {
 	public int calculateTotalPrice(List<Product> products) {
 		int countofA = 0;
 		int priceofA = 50;
+		int countofB = 0;
+		int priceofB = 30;
+		int counterC = 0;
+		int priceofC = 20;
+
 		for (Product product : products) {
 			if ((String.valueOf(product.getSkuid()).equalsIgnoreCase("A")))
 				countofA = countofA + 1;
+			else if ((String.valueOf(product.getSkuid()).equalsIgnoreCase("B")))
+				countofB = countofB + 1;
+			else if ((String.valueOf(product.getSkuid()).equalsIgnoreCase("C")))
+				counterC = counterC + 1;
 		}
 		int totalPriceofA = (countofA / 3) * 130 + (countofA % 3 * priceofA);
-		return totalPriceofA;
+		int totalPriceofB = (countofB / 2) * 45 + (countofB % 2 * priceofB);
+		int totalPriceofC = (counterC * priceofC);
+		return totalPriceofA + totalPriceofB + totalPriceofC;
+
 	}
 }
